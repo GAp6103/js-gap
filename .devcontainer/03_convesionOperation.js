1b8c8e7744e9582d0ec8ceb0907e9de40b028a94
GAp6103 <105291816+GAp6103@users.noreply.github.com>
2024-04-30 19:24:16 +0000

    03_conversionoperation

---
+++ .devcontainer/03_convesionOperation.js
@@ -0,0 +1,15 @@
+let age = 22
+console.log(age)
+console.log(typeof age)
+
+let conversiontostring = String(age)
+
+console.log(typeof conversiontostring)
+console.log(conversiontostring)
+
+//"33"=> 33
+//"33abc"=> NaN
+// true => 1; false=> 0
+// 1=> true ; 0 => false
+// "" => false
+// "gaurav" => true
