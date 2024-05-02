753954fe9803368ca3a6c8e941e670a4b5563187
GAp6103 <105291816+GAp6103@users.noreply.github.com>
2024-05-02 09:25:26 +0000

    good

---
+++ .devcontainer/04_comparison.js
@@ -0,0 +1,10 @@
+console.log("2" > 1)
+console.log(1 >"2")
+console.log(1 >"0.2")
+console.log(null > 0); //comparison operators
+console.log(null == 0); //equality check
+console.log(null >= 0); //comparison operators
+console.log(undefined == 0);
+
+console.log("2" === 2); // triple equal for strict check(data types is also checked)
+
---
+++ .devcontainer/notes.js
@@ -0,0 +1,28 @@
+//two types of data types
+//1. Primitive
+
+//7 types - String,Number,boolean, null, undefined,symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+const turnedoff = false
+const todaywork = null
+const id = Symbol('123')
+
+
+
+//2. reference( non-Primitive)
+
+//array, objects,functions
+
+const heros = ["shaktimann", "naagraj","doga"]
+let myObj = {
+    name: "gaurav",
+    age: 21,
+}
+
+// javascript - dynamic typed language
+
+const myfunc = function(){
+    console.log("Hey my world");
+}
